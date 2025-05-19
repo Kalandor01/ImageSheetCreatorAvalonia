42f323addaefb056785d2a04cd90713f8c6311d9
he <rohovszkyakoska@hmail.com>
2025-05-19 11:03:57 +0200

    works on linux

--- ImageSheetCreatorAvalonia/App.axaml.cs
+++ ImageSheetCreatorAvalonia/App.axaml.cs
@@ -1,9 +1,6 @@
-﻿using Avalonia;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-
-using ImageSheetCreatorAvalonia.ViewModels;
-using ImageSheetCreatorAvalonia.Views;
 
 namespace ImageSheetCreatorAvalonia;
 
@@ -18,7 +15,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
+            desktop.MainWindow = new MainWindow()
             {
                 DataContext = new MainViewModel()
             };
--- ImageSheetCreatorAvalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace ImageSheetCreatorAvalonia.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
